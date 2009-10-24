fd17f21f41ef8ec01842376db50040b864d35b32
Volkan Esgel <vesgel@gmail.com>
2009-10-24 23:03:18 +0300

    copyright info added

--- src/device.py
+++ src/device.py
@@ -1,3 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2009, Kavanoz Authors
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING file.
+#
+
 import dbus
 import gobject
 
