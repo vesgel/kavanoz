e9f97a71a0a238f2780c0047319a3bc525ce3475
Aşkın Yollu <askin@askin.ws>
2010-02-03 01:51:14 +0200

    test file

---
+++ src/kavanoz/test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2009, Kavanoz Authors
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING file.
+#
+
+from dir2dir import dir2dir
+
+d2d = dir2dir("/tmp/kavanoz2/d1", "/tmp/kavanoz2/d2")
+d2d.syncDir("")
