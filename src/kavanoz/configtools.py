0305fc9e8de04d895235ad13c759da8dfbc6fd6c
Volkan Esgel <vesgel@gmail.com>
2010-06-09 20:56:20 +0300

    configtools module is improved.

--- src/kavanoz/configtools.py
+++ src/kavanoz/configtools.py
@@ -14,35 +14,53 @@
 import ConfigParser
 
 class ConfigTool:
-    def __init__(self):
-        self.config_file = "my.conf"
-        
-        self.initialize("write")
+    def __init__(self, config_file):
+        self.config_file = config_file
+        self.config = ConfigParser.RawConfigParser()
 
-    def initialize(self, mode):
-        if mode == "read":
-            self.config = ConfigParser.ConfigParser()
-        elif mode == "write":
-            self.config = ConfigParser.RawConfigParser()
+    def items(self, section):
+	self.readFile()
+	return self.config.items(section)
 
-    def setData(self, section, key, value):
+    def sections(self):
+        self.readFile()
+        return self.config.sections()
+
+    def setValue(self, section, key, value):
         self.config.set(section, key, value)
+        self.writeFile()
+
+    def getValue(self, section, key):
+        self.readFile()
+        return self.config.get(section, key)
+        
+    def getIntValue(self, section, key):
+	self.readFile()
+	return self.config.getInt(section, key)
+	
+    def getFloatValue(self, section, key):
+        self.readFile()
+        return self.config.getFloat(section, key)
+
+    def getBooleanValue(self, section, key):
+        self.readFile()
+        return self.config.getBoolean(section, key)
+
+    def removeData(self, section, key):
+        self.config.remove_option(section, key)
+        self.writeFile()
 
     def addSection(self, section):
         self.config.add_section(section)
+        self.writeFile()
+
+    def removeSection(self, section):
+        self.config.remove_section(section)
+        self.writeFile()
 
     def writeFile(self):
         with open(self.config_file, 'wb') as configfile:
             self.config.write(configfile)
 
-    def readFile(self):        
+    def readFile(self):
         self.config.read(self.config_file)
-
-    def getData(self, section, key):
-        data = self.config.get(section, key)
-        print data
-
-    def removeData(self, section, key):
-        self.config.remove_option(section, key)
-
-    # TODO :: Add moveData function
